6df2736d31f8c9db0d738d5a21053a0eb4dc853b
Daniel Tabirca <dtabirca@gmail.com>
2021-06-21 16:41:35 +0300

    ++

--- CountingElements.php
+++ CountingElements.php
@@ -8,27 +8,41 @@ declare(strict_types=1);
  */
 function solution1(int $X, array $A): int
 {
-	$frogPosition = 0;
-	$leaves = [];
-	if (count($A) === 1) {
-		if ($X === 1) return 0;
-		else return -1;
-	}
-	foreach ($A as $seconds => $leafPosition) {
-		$leaves[$leafPosition] = 1;
-		if (count($leaves) === $X) {
-			return $seconds;
-		}
-	}
-	return -1;
+    $frogPosition = 0;
+    $leaves = [];
+    if (count($A) === 1) {
+        if ($X === 1) return 0;
+        else return -1;
+    }
+    foreach ($A as $seconds => $leafPosition) {
+        $leaves[$leafPosition] = 1;
+        if (count($leaves) === $X) {
+            return $seconds;
+        }
+    }
+    return -1;
 }
 
 /**
  * 2. MaxCounters
  * Calculate the values of counters after applying all alternating operations: increase counter by 1; set value of all counters to current maximum.
  */
-function solution2()
+function solution2(int $N, array $A): array
 {
+    $counters = array_fill(0, $N, 0);
+    $max = 0;
+    $M = count($A);
+    for ($i = 0; $i < $M; $i++) {
+        if ($A[$i] === $N + 1) {
+            $counters = array_fill(0, $N, $max);
+            continue;
+        }
+        $counters[$A[$i] - 1]++;
+        if ($counters[$A[$i] - 1] > $max) {
+        	$max = $counters[$A[$i] - 1];
+        }
+    }
+    return $counters;
 }
 
 /**
@@ -37,6 +51,9 @@ function solution2()
  */
 function solution3()
 {
+    $a = array_flip($A);
+    for ($i = 1; isset($a[$i]); $i++);
+    return $i;
 }
 
 /**
@@ -45,4 +62,12 @@ function solution3()
  */
 function solution4()
 {
+	$N = count($A);
+    $A = array_flip($A);
+    for ($i = 1; $i <= $N; $i++) {
+        if (!isset($A[$i])) {
+            return 0;
+        }
+    }
+    return 1;
 }
